084412e3089fa328c09f7757f43f79b86ce85617
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-04-04 15:28:08 +0600

    query from all nodes

--- python_sdk/tx-example.py
+++ python_sdk/tx-example.py
@@ -10,6 +10,10 @@ if sys.version_info[0] < 3:
 
 IROHA_HOST_ADDR = os.getenv('IROHA_HOST_ADDR', '172.29.101.121')
 IROHA_PORT = os.getenv('IROHA_PORT', '50051')
+IROHA_HOST_ADDR_2 = os.getenv('IROHA_HOST_ADDR', '172.29.101.122')
+IROHA_PORT_2 = os.getenv('IROHA_PORT', '50052')
+IROHA_HOST_ADDR_3 = os.getenv('IROHA_HOST_ADDR', '172.29.101.123')
+IROHA_PORT_3 = os.getenv('IROHA_PORT', '50053')
 ADMIN_ACCOUNT_ID = os.getenv('ADMIN_ACCOUNT_ID', 'admin@test')
 ADMIN_PRIVATE_KEY = os.getenv(
     'ADMIN_PRIVATE_KEY', 'f101537e319568c765b2cc89698325604991dca57b9716b58016b253506cab70')
@@ -18,6 +22,9 @@ ADMIN_PRIVATE_KEY = os.getenv(
 # user_public_key = IrohaCrypto.derive_public_key(user_private_key)
 iroha = Iroha(ADMIN_ACCOUNT_ID)
 net = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR, IROHA_PORT))
+net_2 = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR_2, IROHA_PORT_2))
+net_3 = IrohaGrpc('{}:{}'.format(IROHA_HOST_ADDR_3, IROHA_PORT_3))
+
 
 def trace(func):
     """
@@ -79,7 +86,52 @@ def transfer_coin_from_admin_to_test_user():
     IrohaCrypto.sign_transaction(tx, ADMIN_PRIVATE_KEY)
     send_transaction_and_print_status(tx)
 
+@trace
+def get_account_assets_from_node_1():
+    """
+    List all the assets of 'admin@test'
+    """
+    query = iroha.query('GetAccountAssets', account_id='admin@test')
+    IrohaCrypto.sign_query(query, ADMIN_PRIVATE_KEY)
+
+    response = net.send_query(query)
+    data = response.account_assets_response.account_assets
+    for asset in data:
+        print('Asset id = {}, balance = {}'.format(
+            asset.asset_id, asset.balance))
+
+@trace
+def get_account_assets_from_node_2():
+    """
+    List all the assets of 'admin@test'
+    """
+    query = iroha.query('GetAccountAssets', account_id='admin@test')
+    IrohaCrypto.sign_query(query, ADMIN_PRIVATE_KEY)
+
+    response = net_2.send_query(query)
+    data = response.account_assets_response.account_assets
+    for asset in data:
+        print('Asset id = {}, balance = {}'.format(
+            asset.asset_id, asset.balance))
+
+@trace
+def get_account_assets_from_node_3():
+    """
+    List all the assets of 'admin@test'
+    """
+    query = iroha.query('GetAccountAssets', account_id='admin@test')
+    IrohaCrypto.sign_query(query, ADMIN_PRIVATE_KEY)
+
+    response = net_3.send_query(query)
+    data = response.account_assets_response.account_assets
+    for asset in data:
+        print('Asset id = {}, balance = {}'.format(
+            asset.asset_id, asset.balance))
+
 
 create_asset()
 add_coin_to_admin()
 transfer_coin_from_admin_to_test_user()
+get_account_assets_from_node_1()
+get_account_assets_from_node_2()
+get_account_assets_from_node_3()
